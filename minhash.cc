c895e2fd0867338f6af7f6727c1589369fbc114e
Naoaki Okazaki <okazaki@c.titech.ac.jp>
2025-01-01 16:39:26 +0900

    Removed minhash.h because it is not used by other programs.

--- minhash.cc
+++ minhash.cc
@@ -33,9 +33,9 @@ SOFTWARE.
 #include <utf8.h>
 #include <nlohmann/json.hpp>
 #include <argparse/argparse.hpp>
+#include "MurmurHash3.h"
 
 #include "common.h"
-#include "minhash.h"
 
 //#if !defined(std::byteswap)
 //#include "biteswap.hpp"
@@ -43,9 +43,70 @@ SOFTWARE.
 
 using json = nlohmann::json;
 
+/**
+ * Generate n-grams from a UTF-8 string.
+ *  This function generates n-grams in UTF-8 character (not in byte).
+ *
+ *  @param  str     A string.
+ *  @param  ngrams  A vector of strings to store n-grams.
+ *  @param  n       The number of letters (N).
+ */
+void ngram(const std::string& str, std::vector<std::string>& ngrams, int n)
+{
+    std::vector<const char *> cs;
+    const char *end = str.c_str() + str.size();
+
+    // Do nothing if the given string is empty.
+    if (str.empty()) {
+        return;
+    }
+
+    // Store pointers to the unicode characters in the given string.
+    for (const char *p = str.c_str(); *p; utf8::next(p, end)) {
+        cs.push_back(p);
+    }
+    // Add the pointer to the NULL termination of the string.
+    cs.push_back(end);
+
+    // Append n-grams (note that cs.size() is num_letters + 1).
+    for (int i = 0; i < cs.size()-n; ++i) {
+        const char *b = cs[i];
+        const char *e = cs[i+n];
+        std::string s(b, e-b);
+        ngrams.push_back(s);
+    }
+}
+
+/**
+ * Generate MinHash values for given strings.
+ *  This function generates {num} MinHash values (in uint32_t) by using
+ *  hash functions #{begin}, #{begin+1}, ..., #{begin+num}, and write
+ *  MinHash values to the buffer of {num} * uint32_t bytes.
+ *
+ *  @param  strs    A target item represented as strings.
+ *  @param  out     A pointer to the buffer to receive MinHash values.
+ *  @param  begin   A beginning number of hash functions.
+ *  @param  num     A number of MinHash values to generate.
+ */
+void minhash(const std::vector<std::string>& strs, uint32_t *out, size_t begin, size_t num)
+{
+    for (size_t i = 0; i < num; ++i) {
+        const size_t seed = begin + i;
+        uint32_t min = 0xFFFFFFFF;
+        for (auto it = strs.begin(); it != strs.end(); ++it) {
+            uint32_t hv;
+            MurmurHash3_x86_32(reinterpret_cast<const void*>(it->c_str()), it->size(), seed, &hv);
+            if (hv < min) {
+                min = hv;
+            }
+        }
+        out[i] = min;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int num_items = 0;
+    size_t num_items = 0;
     std::istream& is = std::cin;
     std::ostream& os = std::cout;
     std::ostream& es = std::cerr;
